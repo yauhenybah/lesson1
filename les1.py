6f3d21b9ec7517b5034f25f6c5fb6e0d5443a8f5
yauhenybah <yauheny@gmail.com>
2022-08-29 19:24:56 +0300

    ege

--- les1.py
+++ les1.py
@@ -1,1 +1,12 @@
-print("hello")
+age = input("Введите ваш возраст")
+age1 = int(age)
+if (age1) >= 100:
+    print('you dead ')
+elif  (age1)  >= 5 or (age1) == 10:
+    print('мне ', age1, '  лет')
+elif (age1) > 1 and (age1) < 5:
+    print('мне ', age1, 'года')
+elif (age1) == 1:
+    print('мне ', age1, 'год')
+else:
+    print()
